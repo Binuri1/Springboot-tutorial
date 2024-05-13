3ab74e8c7955a7f419d86ea6c62f036f26851a1d
Binuri Mindula <binuriwagarachchi@gmail.com>
2024-05-13 15:08:16 +0530

    Created new springboot project

---
+++ New-springboot/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/User")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+}
