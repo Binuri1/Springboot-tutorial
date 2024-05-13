a3ce90500f454b476ea2dc89e88f53743be9a789
Binuri Mindula <binuriwagarachchi@gmail.com>
2024-05-13 13:58:36 +0530

    Created new springboot project

---
+++ New-springboot/src/main/java/com/example/demo/NewSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSpringbootApplication.class, args);
+	}
+
+}
