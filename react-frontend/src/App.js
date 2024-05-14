48cc0dc64b2d2f7f80ca60dfd9393a7953e3a50b
Binuri Mindula <binuriwagarachchi@gmail.com>
2024-05-14 15:12:05 +0530

    Update App.js  with routes

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,12 +2,21 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
+import AddUser from "../users/AddUser";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-       <Navbar/>
-       <Home/>
+      <Router>
+          <Navbar/>
+          <Routes>
+            <Route exact path="/" element={<Home/>}></Route>    //when only type "/" with localhost:3000 it goes to home page
+            <Route exact path="/adduser" element={<AddUser/>}></Route> 
+          </Routes>
+          
+      </Router>
+      
        
     </div>
   );
