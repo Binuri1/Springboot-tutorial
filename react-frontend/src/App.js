c3e13bd6d7ec71f8db047db3d9ea86ff92a2e7ed
Binuri Mindula <binuriwagarachchi@gmail.com>
2024-05-15 03:06:54 +0530

    Update App.js

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
-import AddUser from "./users/AddUser";
+import AddUser from './users/AddUser';
+import EditUser from './users/EditUser';
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home/>}></Route>    //when only type "/" with localhost:3000 it goes to home page
             <Route exact path="/adduser" element={<AddUser/>}></Route> 
+            <Route exact path="/edituser/:id" element={<EditUser/>}></Route> 
           </Routes>
           
       </Router>
