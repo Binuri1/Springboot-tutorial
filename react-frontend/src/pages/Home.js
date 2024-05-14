54264afee306e924c86e114b5185bff2d508bf4c
Binuri Mindula <binuriwagarachchi@gmail.com>
2024-05-14 12:33:05 +0530

    Create Home.js

--- react-frontend/src/pages/Home.js
+++ react-frontend/src/pages/Home.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Home() {
+
+  const [users,setUsers]=useState([])  //create object for store user info using useState hook....initial state [](empty array)
+
+  useEffect(()=>{                            //useEffect hook use to tell the react that my component needs to do something after render..
+                                             //so every time page is open so it will load the user info
+        loadUsers();
+
+  },[]);        //if we not pass empty array this will run unlimited time
+
+  const loadUsers=async()=>{                  //load users 
+                                               //use async bcz JS execute line by line in here we want to execute all the lines to get all users
+        
+     const result =await axios.get("http://localhost:8080/users") //await use to wait here
+     console.log(result)
+  };
+
+                                        
+  
   return (
     <div className='container'>
         <div className='py-4'>
