27290105196c4197f25ba8cf88d971b6d3af4e9c
Binuri Mindula <binuriwagarachchi@gmail.com>
2024-05-14 15:17:01 +0530

    Update Navbar

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
-import AddUser from "../users/AddUser";
+import AddUser from "./users/AddUser";
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 function App() {
